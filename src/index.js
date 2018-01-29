373d0dcc7965ea52a585080d90eb0641f2798633
Rocciescobar <roccisen.88@gmail.com>
2018-01-28 20:27:58 -0500

    funcion que agrega figcaption y alt de imagen

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@
 (() => {
   const containers = $('.container').find('img');
   containers.wrap('<figure></figure>');
-})()
+
+  // Agrega etiqueta figcation con alt de imagen
+  $(containers).each(function() {
+    $(this).after('<figcaption>' + $(this).attr('alt') + '</figcaption>');
+  });
+})();
