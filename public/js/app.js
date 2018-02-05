4d3d6356911b19b1289bdf5ca8edb8e30df3f324
Manuela <fmanuela499@gmail.com>
2018-02-04 23:25:30 -0500

    actualizando index.html

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,17 @@
 'use strict';
 
 window.addEventListener('load', function () {
-  var config = 'contrast(30%)';
+  var config = 'grayscale(50%)';
   $('.container').cardify(config);
+
+  var start1 = $('.start-1-js');
+  var start2 = $('.start-2-js');
+
+  start1.on('click', function () {
+    window.location.href = 'views/gallery.html';
+  });
+
+  start2.on('click', function () {
+    window.location.href = 'views/gallery2.html';
+  });
 });
